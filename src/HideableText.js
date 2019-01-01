e4ccdcd14c12903b88feda4e74de0d3578f5adfb
pracniscate <lorena.giddens@outlook.com>
2019-01-01 12:09:03 -0500

    the toggle button can hide the text

--- src/HideableText.js
+++ src/HideableText.js
@@ -3,13 +3,24 @@ import React from 'react';
 export default class HideableText extends React.Component {
   constructor (props) {
     super(props); // calls the parent class constructor
+    this.state = {
+      isHidden: false,
+    }
+  }
+
+  // the 'setState' React.Component method will return an object
+  toggleIsHidden () {
+    this.setState((currentState) => ({
+      isHidden: !currentState.isHidden,
+    }));
   }
 
   render () {
     return (
       <div>
-        <button>Toggle</button>
-        {this.props.text}
+        <button onClick={() => this.toggleIsHidden()}>Toggle</button>
+        {/* the text will only show if 'isHidden' is true */}
+        {!this.state.isHidden && this.props.text}
       </div>
     );
   }
