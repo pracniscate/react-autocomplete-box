882ee5520d3c84491adbcc4094c397da7e03e43f
pracniscate <lorena.giddens@outlook.com>
2019-01-01 12:28:48 -0500

    start a search box with a list of names

--- src/App.js
+++ src/App.js
@@ -2,28 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HideableText from './HideableText';
+import AutocompleteText from './AutoCompleteText'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello World
-          </p>
-          <div className="App-content">
-            <HideableText text="Dynamic Text!" />
-          </div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <AutocompleteText />
+        {/* <HideableText text="Dynamic Text!" /> */}
       </div>
     );
   }
